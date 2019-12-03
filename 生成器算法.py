c00e22e61b3a3c2d64e7e3b8f51eecb7ad985d24
Jack Jiang <jackjyq@outlook.com>
2019-12-03 20:50:43 +1100

    适配宽屏显示器

--- 生成器算法.py
+++ 生成器算法.py
@@ -5,8 +5,7 @@ from os import listdir
 class 生成器:
     def __init__(self, 随机种子:int=None)->None:
         random.seed(随机种子)
-        self.主题 = {"主题动词": "树立", "主题名词": "理想"}
-        self.模版 = self.读取文件("./模版/模版.txt")
+        self.模版 = self.读取文件("./模版库/模版.txt")
         self.语料类型大全 = self.获取语料类型()
         self.语料库 = {}
         for 语料类型 in self.语料类型大全:
@@ -42,22 +41,32 @@ class 生成器:
             语料计数[语料类型] += 1
         return 段落
 
-    def 生成作文(self)->list:
-        作文 = []
-        模版 = self.模版
-        # 洗牌和语料计数可防止重复选取同一条语料
-        self.语料库洗牌()
+    def 初始化语料计数(self)->dict:
         语料计数 = {}
         for 语料类型 in self.语料类型大全:
             语料计数[语料类型] = 0
+        return 语料计数
+
+    def 生成作文(self, 主题谓语:str="", 主题宾语:str="")->list:
+        # 随机选择模版
+        模版 = self.模版
+        # 随机替换语料
+        初稿 = []
+        self.语料库洗牌()
+        语料计数 = self.初始化语料计数()
         for 段落 in 模版:
             for 语料类型 in self.语料类型大全:
                 段落 = self.应用语料(段落, 语料计数, 语料类型)
-            段落 = 段落.replace("「主题动词」", self.主题["主题动词"])
-            段落 = 段落.replace("「主题名词」", self.主题["主题名词"])
-            作文.append(段落)
-        return 作文
+            初稿.append(段落)
+        # 替换主题词
+        定稿 = []
+        for 段落 in 初稿:
+            段落 = 段落.replace("「主题谓语」", 主题谓语)
+            段落 = 段落.replace("「主题宾语」", 主题宾语)
+            定稿.append(段落)
+        return 定稿
 
+# 测试代码
 if __name__ == "__main__":
     生成器 = 生成器()
-    print(生成器.生成作文())
+    print(生成器.生成作文("积极", "尝试"))
