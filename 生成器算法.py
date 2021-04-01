5132281ac1ab2804a9cb40ec3efa668c27b82e92
Yiqun (Jack) Jiang <jackjyq@outlook.com>
2021-04-01 15:40:09 +1100

    增加历史记录功能

--- 生成器算法.py
+++ 生成器算法.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-import random
+import random, sqlite3
 from os import listdir
 
 class 生成器:
@@ -14,6 +14,7 @@ class 生成器:
         for 语料名称 in self.语料列表:
             self.语料库[语料名称] = self.读取文件("./语料库/"+语料名称+".txt")
         self.作文总数 = self.计算作文总数()
+        self.数据库 = 数据库()
 
     def 读取文件(self, 文件路径:str)->list:
         数据 = []
@@ -68,7 +69,12 @@ class 生成器:
             段落 = 段落.replace("「主题谓语」", 主题谓语)
             段落 = 段落.replace("「主题宾语」", 主题宾语)
             定稿.append(段落)
+        # 记录数据库
+        self.数据库.写入数据库(谓语=主题谓语, 宾语=主题宾语)
         return 定稿
+    
+    def 生成记录(self)->list:
+        return self.数据库.读取数据库()
     
     def 计算作文总数(self)->int:
         """计算能够生成的作文总数
@@ -93,6 +99,44 @@ class 生成器:
             for i in range(语料选择次数):
                 模版作文总数 *= 语料数量 - i
         return 模版作文总数
+    
+
+class 数据库:
+    def __init__(self)->None:
+        # 初始化数据库
+        # 谓语 宾语 生成次数
+        # 勇于 尝试 10
+        # 积极 进去 2
+        with sqlite3.connect("数据库.sqlite", check_same_thread=False) as self.数据库连接:
+            self.数据库句柄 = self.数据库连接.cursor()
+            self.数据库句柄.execute("""CREATE TABLE IF NOT EXISTS 生成记录 (
+                    谓语 TEXT,
+                    宾语 TEXT,
+                    生成次数 INTEGER DEFAULT 1
+                    );""")
+
+    def 写入数据库(self, 谓语:str="", 宾语:str="")->None:
+        # 生成次数 ++
+        self.数据库句柄.execute("""SELECT 生成次数 FROM 生成记录
+                WHERE 谓语=? AND 宾语=?""", (谓语, 宾语))
+        生成次数 = self.数据库句柄.fetchall()
+        if len(生成次数):
+            更新生成次数 = 生成次数[0][0] + 1
+            self.数据库句柄.execute("""UPDATE 生成记录
+                    SET 生成次数 =?
+                    WHERE 谓语=? AND 宾语=?"""
+                    , (更新生成次数, 谓语, 宾语))
+        else:
+            self.数据库句柄.execute("""INSERT INTO 生成记录(谓语, 宾语, 生成次数)
+                    VALUES(?,?,?)""", (谓语, 宾语, 1))
+        self.数据库连接.commit()
+
+    def 读取数据库(self)->list:
+        # 生成记录按照生成次数排序
+        self.数据库句柄.execute("""SELECT * FROM 生成记录
+                ORDER BY 生成次数 DESC""")
+        生成记录 = self.数据库句柄.fetchall()
+        return 生成记录
 
 # 测试代码
 if __name__ == "__main__":
