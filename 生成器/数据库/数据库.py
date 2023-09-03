8de82a4cd15f76b2e5744ba6fc7feebe53892166
Jack Jiang <jackjyq@outlook.com>
2023-09-03 17:31:50 +0800

    使用二进制保存向量，以节省空间

--- 生成器/数据库/数据库.py
+++ 生成器/数据库/数据库.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 import pathlib
+import pickle
 import sys
 from datetime import datetime
 from typing import Optional
@@ -10,7 +11,7 @@ if 生成器目录 not in sys.path:
     sys.path.insert(0, 生成器目录)
 from pathlib import Path
 
-from sqlalchemy import JSON, Column, DateTime, String, create_engine
+from sqlalchemy import JSON, Column, DateTime, String, create_engine, LargeBinary
 from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy.sql import func
 
@@ -35,14 +36,15 @@ class Sbert_base_chinese_nli_embeddings(Base):
 
 
 class Text_embedding_ada_002_embeddings(Base):
-    """
+    """OpenAI Text Embedding
+
     最长大约 8191 token, 设成500以避免消耗太多 API
     https://platform.openai.com/docs/guides/embeddings/what-are-embeddings
     """
 
     __tablename__ = "Text_embedding_ada_002_embeddings"
     sentense = Column(String(length=500), primary_key=True)
-    embeddings = Column(JSON, nullable=False)
+    embeddings = Column(LargeBinary)
     timestamp = Column(
         DateTime(timezone=False),
         default=datetime.utcnow,
@@ -52,8 +54,8 @@ class Text_embedding_ada_002_embeddings(Base):
 
 
 class 数据库类:
-    def __init__(self) -> None:
-        self.数据库路径: Path = Path(__file__).parent / "数据库.sqlite"
+    def __init__(self, database: str = "数据库.sqlite") -> None:
+        self.数据库路径: Path = Path(__file__).parent / database
         self.engine = create_engine(f"sqlite+pysqlite:///{self.数据库路径}", echo=False)
         self.Session = sessionmaker(self.engine)
         Base.metadata.create_all(self.engine)
@@ -62,7 +64,7 @@ class 数据库类:
         # return None means not present in database
         with self.Session() as session:
             if result := session.get(Sbert_base_chinese_nli_embeddings, 文本):
-                return result.embeddings
+                return result.embeddings  # type: ignore
         return None
 
     def 插入特征向量_sbert_base_chinese_nli(self, 文本: str, 向量: list[float]):
@@ -71,15 +73,21 @@ class 数据库类:
             session.commit()
 
     def 读取特征向量_text_embedding_ada_002(self, 文本) -> Optional[list[float]]:
+        """二进制向量会转换成 object"""
         # return None means not present in database
         with self.Session() as session:
             if result := session.get(Text_embedding_ada_002_embeddings, 文本):
-                return result.embeddings
+                return pickle.loads(result.embeddings)  # type: ignore
         return None
 
     def 插入特征向量_text_embedding_ada_002(self, 文本: str, 向量: list[float]):
+        """向量会转换成二进制保存，以节省空间"""
         with self.Session() as session:
-            session.add(Text_embedding_ada_002_embeddings(sentense=文本, embeddings=向量))
+            session.add(
+                Text_embedding_ada_002_embeddings(
+                    sentense=文本, embeddings=pickle.dumps(向量)
+                )
+            )
             session.commit()
 
 
@@ -87,55 +95,3 @@ if __name__ == "__main__":
     数据库: 数据库类 = 数据库类()
     特征向量 = 数据库.读取特征向量_text_embedding_ada_002("小嘿")
     print(type(特征向量), 特征向量)
-
-# class 数据库类:
-#     def __init__(self) -> None:
-#         # 初始化数据库
-#         # 谓语 宾语 生成次数
-#         # 勇于 尝试 10
-#         # 积极 进去 2
-#         self.基础路径 = path.abspath(path.dirname(__file__))
-#         with sqlite3.connect(
-#             self.基础路径 + "/数据库.sqlite", check_same_thread=False
-#         ) as self.数据库连接:
-#             self.数据库句柄 = self.数据库连接.cursor()
-#             self.数据库句柄.execute(
-#                 """CREATE TABLE IF NOT EXISTS 生成记录 (
-#                     谓语 TEXT,
-#                     宾语 TEXT,
-#                     生成次数 INTEGER DEFAULT 1
-#                     );"""
-#             )
-
-#     def 写入数据库(self, 谓语: str = "", 宾语: str = "") -> None:
-#         # 生成次数 ++
-#         self.数据库句柄.execute(
-#             """SELECT 生成次数 FROM 生成记录
-#                 WHERE 谓语=? AND 宾语=?""",
-#             (谓语, 宾语),
-#         )
-#         生成次数 = self.数据库句柄.fetchall()
-#         if len(生成次数):
-#             更新生成次数 = 生成次数[0][0] + 1
-#             self.数据库句柄.execute(
-#                 """UPDATE 生成记录
-#                     SET 生成次数 =?
-#                     WHERE 谓语=? AND 宾语=?""",
-#                 (更新生成次数, 谓语, 宾语),
-#             )
-#         else:
-#             self.数据库句柄.execute(
-#                 """INSERT INTO 生成记录(谓语, 宾语, 生成次数)
-#                     VALUES(?,?,?)""",
-#                 (谓语, 宾语, 1),
-#             )
-#         self.数据库连接.commit()
-
-#     def 读取数据库(self) -> list:
-#         # 生成记录按照生成次数排序
-#         self.数据库句柄.execute(
-#             """SELECT * FROM 生成记录
-#                 ORDER BY 生成次数 DESC"""
-#         )
-#         生成记录 = self.数据库句柄.fetchall()
-#         return 生成记录
